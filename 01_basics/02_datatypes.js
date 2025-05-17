d4fdcd1372a49d7f2d705c43d1f4a7915717a9ee
Abhimanyu <114327346+abhi-techie529@users.noreply.github.com>
2025-05-17 09:21:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)   // we are using nodejs , not browser so it create error here but in browser it will show us a pop of "6"
+
+console.log(3
+    +
+     3);  //code readability should be high , dont write like this
+
+console.log("Abhimanyu");
+
+
+let name="Abhimanyu"
+let age = 18
+let isLoggedIn =  false
+let state = null;
+let city;
+
+//*DATATYPES* //
+//number => 2 to power 53
+// string => ""
+// boolean => true/false
+// null => standalone value , means empty for ex: we analyse temperatue but it is 0 degrree celcius so instead of 0 we prefer it is "null"
+// undefined => 
+// symbol => unique
+
+
+//*OBJECT* //
+
+console.log(typeof "Abhimanyu");
+console.log(typeof age);
+console.log(typeof undefined);  //undefined
+console.log(typeof null);      //object
+
+
+
