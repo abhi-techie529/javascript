6edd3e4dad7145c312448bbb81510e3a6a399592
Abhimanyu <114327346+abhi-techie529@users.noreply.github.com>
2025-05-31 12:51:16 +0000

    half code for function file

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,47 @@
+//******** Function **********//
+
+function sayMyName(){
+    console.log("A");
+    console.log("B");
+    console.log("H");
+    console.log("I");
+    console.log("M");
+    console.log("A");
+    console.log("N");
+    console.log("Y");
+    console.log("U");
+    
+}
+// sayMyName()            // this syntax , help to exwcute the function simple syntax
+
+//**************************************//
+
+
+// function addTwoNumbers(number1 , number2){
+//     console.log(number1 + number2);
+// }
+
+// addTwoNumbers(4 ,5)   // output :- 9
+// addTwoNumbers(4 ,"5")   // output :- 45 , its work as string
+// addTwoNumbers(4 ,null)   // output :- 4 , 
+
+
+//********************************************//
+
+function addTwoNumbers(number1 , number2){
+ 
+    //    let result = number1 + number2       // first way to write a code for return , this "result " we declare inside addTwoNumbers 
+    //    return result
+    //    console.log("Abhi");    // this is functions br default rule:- that after return no function will work
+       
+    return number1 + number2
+}
+
+const result = addTwoNumbers(4 ,5)    // this "result " we declare outside the  above {}function
+
+console.log("Result:" , result );
+
+
+//*********** New Function  ************//
+
+
